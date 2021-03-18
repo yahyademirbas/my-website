6730677fa0f8ed7261631a28fd72691cfe3879bc
yahyademirbas <yahyademirbas@yahoo.com>
2021-03-19 01:55:52 +0300

    final fix

--- src/components/pages/Works.js
+++ src/components/pages/Works.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Project from '../layouts/Project'
-import { projects, miscellaneous, section3Title, section4Title } from '../../profile'
+import { projects, /*miscellaneous, */section3Title, /*section4Title */} from '../../profile'
 
 const Works = () => {
     return (
         <>
             <div data-aos="zoom-in-up" data-aos-once="true" className="third">
-                <center>
+
                     <div className="pp-head-line">
                         <h1 id="Projects" className="red-line pp-head">{section3Title}</h1>
                     </div>
-                </center>
+
                 <div className="row">
                 {projects && projects.map((x) =>
                 <Project id={x.id} url={x.url} name={x.name} skills={x.skills}/>
