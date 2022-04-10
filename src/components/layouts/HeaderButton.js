1f8c9abcdc92d7450371c28f8777b2665a1943c4
Yahya Demirbas <74156901+yahyademirbas@users.noreply.github.com>
2022-04-10 19:10:18 +0300

    Update HeaderButton.js

--- src/components/layouts/HeaderButton.js
+++ src/components/layouts/HeaderButton.js
@@ -5,7 +5,7 @@ const HeaderButton = () => {
     return (
         <center>
             <div>
-                <a href="https://www.youtube.com/watch?v=wWhF4C4aPio"><button className="see">My Last Video<i className="fas fa-eye"></i></button></a>
+                <a href="https://www.youtube.com/watch?v=JABsWvf6FyQ"><button className="see">My Last Video<i className="fas fa-eye"></i></button></a>
             </div>
         </center>
     )
